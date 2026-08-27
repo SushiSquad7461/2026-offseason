ad1d45f30702c0880e0009ed813015fb16f2774a
SVkanchan <surabhikanch@gmail.com>
2026-08-27 13:04:35 -0700

    Update TeleopSwerve.java

--- src/main/java/frc/robot/subsystem/TeleopSwerve.java
+++ src/main/java/frc/robot/subsystem/TeleopSwerve.java
@@ -9,7 +9,7 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 SparkMax spark = new SparkMax(1, MotorType.kBrushless);
 
-
+SparkMax spark = new SparkMax(1, MotorType.kBrushless);
 public class TeleopSwerve extends Command {    
     private Swerve s_Swerve;    
     private DoubleSupplier translationSup;
