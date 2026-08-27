c490d591e692b1117adee33135fdc1ea1dcbb12d
Masab <masab.khateeb@gmail.com>
2026-08-26 23:10:35 -0700

    Added stop and removed logging

--- src/main/java/frc/robot/subsystems/turret/TurretIOSim.java
+++ src/main/java/frc/robot/subsystems/turret/TurretIOSim.java
@@ -1,36 +1,35 @@
 package frc.robot.subsystems.turret;
 
-import org.littletonrobotics.junction.AutoLog;
 import edu.wpi.first.math.MathUtil;
 
-public class TurretIOSim implements TurretIO{
-    
-    @AutoLog
-    public static class ShooterData{
-        public double appliedVolts = 0.0;
-        public double currentAmps = 0.0;
-    }
-    
-    public final ShooterData data = new ShooterData();
+public class TurretIOSim implements TurretIO {
 
-    private double simulatedDegrees = 0; 
+    private double simulatedDegrees = 0;
     private double targetDegrees = 0;
 
     @Override
-    public void turnDegrees(double degrees){
-        targetDegrees = MathUtil.clamp(simulatedDegrees + degrees, 0, 360);
-        // implement?
-        //data.appliedVolts = ;
+    public void turnDegrees(double degrees) {
+        targetDegrees = MathUtil.clamp(
+                simulatedDegrees + degrees,
+                0,
+                360);
+
+        simulatedDegrees = targetDegrees;
     }
 
     @Override
-    public double getCurrentAngle(){
+    public double getCurrentAngle() {
         return simulatedDegrees;
     }
 
     @Override
-    public void moveToCenter(){
+    public void moveToCenter() {
         targetDegrees = 0;
+        simulatedDegrees = targetDegrees;
     }
 
+    @Override
+    public void stop() {
+        targetDegrees = simulatedDegrees;
+    }
 }
