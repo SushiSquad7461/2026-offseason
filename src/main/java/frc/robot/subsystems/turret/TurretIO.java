e6a8d713b87fb0a22e8f2eb90a922633f8a8e8e0
Masab <masab.khateeb@gmail.com>
2026-08-26 23:08:20 -0700

    added stop to interface

--- src/main/java/frc/robot/subsystems/turret/TurretIO.java
+++ src/main/java/frc/robot/subsystems/turret/TurretIO.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems.turret;
 
 public interface TurretIO {
     void turnDegrees(double degrees);
+    void stop();
     double getCurrentAngle();
     void moveToCenter();
 }
